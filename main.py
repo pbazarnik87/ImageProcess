d947e79d793af204da6556097e6eaff3c3ac127f
pbazarnik87 <pbazarnik87@gmail.com>
2021-02-24 23:39:27 +0100

    update

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
-import logging
 import cv2 as cv
-import sys
 import basic_operations
-import os
-import numpy as np
-
 
 
 def main():
+    """
+    main
+    """
 
-
-#---------------------------------------------------------------------------------------------------
-    # show original
+    '#show original'
     original = basic_operations.load_image(r'pic\face.png')
     cv.imshow("original", original)
 
     # color2gray
-    gray =  basic_operations.convert_bgr_to_gray(r'pic\face.png')
-    cv.imshow("gray",gray)
+    gray = basic_operations.convert_bgr_to_gray(r'pic\face.png')
+    cv.imshow("gray", gray)
 
     # blur
-    blur = cv.GaussianBlur(gray,(5,5),0)
+    blur = cv.GaussianBlur(gray, (5, 5), 0)
     cv.imshow("blur", blur)
 
-    # otsu
+    '#otsu'
     otsu = basic_operations.otsu(blur)
     otsu_img = otsu[1]
     cv.imshow("otsu", otsu_img)
 
-
-    # edges Canny
-    edges = cv.Canny(otsu_img,0,200)
+    '#edges Canny'
+    edges = cv.Canny(otsu_img, 0, 200)
     cv.imshow("canny", edges)
 
-    # count circles
-    cir = basic_operations.circles_detect(original,edges)
+    '#count circles'
+    cir = basic_operations.circles_detect(original, edges)
     cv.imshow('detected circles', cir)
     cv.waitKey(0)
     cap = cv.VideoCapture(0)
@@ -63,6 +58,7 @@ def main():
     # When everything done, release the capture
     cap.release()
     cv.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
